638cf7e014a6ac0bd691b7f9bb2ff7d2b5a490dd
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2014-01-14 14:19:33 +0100

    Wactor is a library, not a standalone application.

--- test/wactor_test.erl
+++ test/wactor_test.erl
@@ -9,12 +9,11 @@ session_test_() ->
       [fun test_locker/0]}].
 
 test_setup() ->
-    application:start(wactor),
+    wactor_sup:start_link(),
     wactor_locker:start([node()]).
 
 test_teardown(_) ->
-    application:stop(wactor).
-
+    ok.
 
 test_locker() ->
     {ok, _Pid} = wactor_locker:start_channel(hej, [{act1, wactor_example_count, []}]),
